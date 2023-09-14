abfdfbf8f84dd634cb648b4354b5c33dc2381373
TeemuS23 <teemu23017@student.hamk.fi>
2023-09-14 16:13:09 +0300

    Initial commit

---
+++ src/app/TulostinOhjelma.java
@@ -0,0 +1,30 @@
+package app;
+
+public class TulostinOhjelma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String tekija="Teemu";
+		double luku1=(5.0);
+		double luku2=(2.0);
+		double tulo=(luku1*luku2);
+		
+		System.out.println("Hei olen Tulostin-ohjelma");
+		System.out.print("Ohjelman tekijä: ");
+		System.out.println(tekija);
+		
+		//System.out.println(luku1);
+		
+		System.out.print("Luku1-muuttujan arvo on ");
+		System.out.println(luku1);
+		System.out.print("Luku2-muuttujan arvo on ");
+		System.out.println(luku2);
+		System.out.print(luku1 + "*" + luku2 + "=" + tulo);
+		
+		
+		
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module TulostinHarjoitus {
+}
